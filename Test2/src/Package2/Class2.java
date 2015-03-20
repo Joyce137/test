9734e71af5fbfc0825ae9756a45b74ec7910c3d8
Joyce137 <1010815793@qq.com>
2015-03-20 14:52:52 +0800

    Test2_commit

---
+++ Test2/src/Package2/Class2.java
@@ -0,0 +1,37 @@
+package Package2;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.WebDriver.*;
+import org.openqa.selenium.firefox.*;
+import org.openqa.selenium.ie.*;
+import org.openqa.selenium.chrome.*;
+
+public class Class2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		
+		//----Firefox-----
+		//如果啟動出現問題，可以使用System.setProperty指出firefox.exe的路徑
+		//System.setProperty("webdriver.firefox.bin", "D:\\Mozilla Firefox\\firefox.exe");
+		//WebDriver dirver_firefox = new FirefoxDriver();
+		
+		//----IE-----
+		//如果啟動出現問題，可以使用System.setProperty指出webdriver.ie.dirver的路徑,可以從http://docs.seleniumhq.org/download/下載
+		//System.setProperty("webdriver.ie.driver", "D:\\安装包\\测试Test\\IEDriverServer.exe");
+		//WebDriver dirver_ie = new InternetExplorerDriver();
+		
+		//----Chrome-----
+		//如果啟動出現問題，可以使用System.setProperty指出webdriver.chrome.dirver的路徑,可以從http://docs.seleniumhq.org/download/下載
+		System.setProperty("webdriver.chrome.driver", "D:\\安装包\\测试Test\\chromedriver.exe");
+		WebDriver dirver_chrome = new ChromeDriver();
+		
+		Navigation navigation = dirver_chrome.navigate();
+		navigation.to("http://www.baidu.com");
+	}
+
+}
