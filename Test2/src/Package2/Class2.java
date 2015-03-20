c13270cc8313338b48bb2c9a0e9cece1642c4040
Joyce137 <1010815793@qq.com>
2015-03-20 15:08:24 +0800

    Test2_commit_1

--- Test2/src/Package2/Class2.java
+++ Test2/src/Package2/Class2.java
@@ -14,24 +14,49 @@ public class Class2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+
+		// ----Firefox-----
+		// 如果啟動出現問題，可以使用System.setProperty指出firefox.exe的路徑
+		// System.setProperty("webdriver.firefox.bin",
+		// "D:\\Mozilla Firefox\\firefox.exe");
+		// WebDriver dirver_firefox = new FirefoxDriver();
+
+		// ----IE-----
+		// 如果啟動出現問題，可以使用System.setProperty指出webdriver.ie.dirver的路徑,可以從http://docs.seleniumhq.org/download/下載
+		System.setProperty("webdriver.ie.driver",
+				"D:\\安装包\\测试Test\\IEDriverServer.exe");
+		WebDriver dirver_ie = new InternetExplorerDriver();
+
+		// ----Chrome-----
+		// 如果啟動出現問題，可以使用System.setProperty指出webdriver.chrome.dirver的路徑,可以從http://docs.seleniumhq.org/download/下載
+		// System.setProperty("webdriver.chrome.driver",
+		// "D:\\安装包\\测试Test\\chromedriver.exe");
+		// WebDriver dirver_chrome = new ChromeDriver();
+
+		Navigation navigation = dirver_ie.navigate();
+		String dev = "https://dev.zoom.us";
+		String www = "https://www.zoom.us";
+		navigation.to(dev);
+		navigation.to(www);
+
+		//空閒3秒
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		navigation.back();
 		
-		//----Firefox-----
-		//如果啟動出現問題，可以使用System.setProperty指出firefox.exe的路徑
-		//System.setProperty("webdriver.firefox.bin", "D:\\Mozilla Firefox\\firefox.exe");
-		//WebDriver dirver_firefox = new FirefoxDriver();
-		
-		//----IE-----
-		//如果啟動出現問題，可以使用System.setProperty指出webdriver.ie.dirver的路徑,可以從http://docs.seleniumhq.org/download/下載
-		//System.setProperty("webdriver.ie.driver", "D:\\安装包\\测试Test\\IEDriverServer.exe");
-		//WebDriver dirver_ie = new InternetExplorerDriver();
-		
-		//----Chrome-----
-		//如果啟動出現問題，可以使用System.setProperty指出webdriver.chrome.dirver的路徑,可以從http://docs.seleniumhq.org/download/下載
-		System.setProperty("webdriver.chrome.driver", "D:\\安装包\\测试Test\\chromedriver.exe");
-		WebDriver dirver_chrome = new ChromeDriver();
-		
-		Navigation navigation = dirver_chrome.navigate();
-		navigation.to("http://www.baidu.com");
+		//空閒3秒
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		navigation.forward();
+		navigation.refresh();
 	}
 
 }
